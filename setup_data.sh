59172775c4293d5cfcaf949eb6336832ddc79680
Henry Read <henrya2@126.com>
2018-10-28 23:16:11 +0800

    remove -s option ...

--- setup_data.sh
+++ setup_data.sh
@@ -4,4 +4,4 @@ SCRIPT=$( readlink -m ${BASH_SOURCE[0]} ) # Full path to script
 BASE_DIR=`dirname ${SCRIPT}` # Directory script is run in
 
 # Symlink mounted data to their destinations
-ln -s /floyd/input/cifar_10_batches_py ${BASE_DIR}/assignments/assignment1/cs231n/datasets/
+ln /floyd/input/cifar_10_batches_py ${BASE_DIR}/assignments/assignment1/cs231n/datasets/
