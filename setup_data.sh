b8cb2d2c7c27a5108015e4fa298965b9c9557b4e
Henry Read <henrya2@126.com>
2018-10-28 22:58:01 +0800

    ...............

--- setup_data.sh
+++ setup_data.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+SCRIPT=$( readlink -m ${BASH_SOURCE[0]} ) # Full path to script
+BASE_DIR=`dirname ${SCRIPT}` # Directory script is run in
+
 # Symlink mounted data to their destinations
-ln -s /floyd/input/cifar_10_batches_py /floyd/home/assignments/assignment1/cs231n/datasets/
+ln -s /floyd/input/cifar_10_batches_py ${BASE_DIR}/assignments/assignment1/cs231n/datasets/
